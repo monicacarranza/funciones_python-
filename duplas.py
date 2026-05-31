fc29f3dda270c3b3ca450a2532129c2f9eb0d2e0
monicacarranza <4235571@clases.edu.sv>
2026-05-31 14:02:26 -0600

    Create duplas.py

---
+++ duplas.py
@@ -0,0 +1,8 @@
+#Tuplas muy similares a las listas
+#No pueden modificarse
+#Se utilizan parentesis en lugar de  {}
+
+lista = {"monica", "karla"} #Constantemente agregado informacio
+tupla = {"monica", "karla"}
+
+videojuegos = { }
