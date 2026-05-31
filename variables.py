49df541a744e745902fc1bc0a43da6ea53b9f080
monicacarranza <4235571@clases.edu.sv>
2026-05-31 14:03:07 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+#soy un comentario
+#strings, cadenas, textos
+#numericos int (intenger, entero) float (punto flotante)
+nombre = 'monica'
+apellido = 'carranza'
+edad = 18
+
+print(nombre, apellido, edad)
+
+#edad = edad /3
+
+print(nombre, apellido, edad)
+
+print(type(edad))
