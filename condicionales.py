04c4579ba582e9167de5decd2b29458c013755e7
monicacarranza <4235571@clases.edu.sv>
2026-05-31 13:15:17 -0600

    Create condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+#Listas de elementos llave:valor
+#Arreglos asociativos
+
+persona = {"nombre": "Jeymi",
+           "edad": 20,
+           "apellido": "Zapata"}
+
+persona["apodo"] = "Ringa Tech"
+
+#print(persona.items())
+
+print("direccion" in persona)
+
+#for key, value in persona.items():
+#    print(f" La llave {Key} tiene el valor {value}")
