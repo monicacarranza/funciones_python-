64a1d95cac9361059843afb2a608857de9c0bc04
monicacarranza <4235571@clases.edu.sv>
2026-05-31 13:26:40 -0600

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,17 @@
+#Pre-definidas print(), type(), range(), input()
+
+def saludar(nombre):
+    print("Hola", nombre)
+
+#Celsius a fahrenheit: (C* 1.8) + 32
+def convertir_a_fanhrenheit(c):
+    return (c * 1.8) + 33
+
+#saludar("Jeymi")
+#saludar("Katya")
+#saludar("Yenifer")
+
+print(convertir_a_fanhrenheit(10))
+print(convertir_a_fanhrenheit(20))
+print(convertir_a_fanhrenheit(30))
+
