6976d6660ddf6e8184d710876be5b206c9dfc9b0
monicacarranza <4235571@clases.edu.sv>
2026-05-31 13:28:14 -0600

    Create funciones2.py

---
+++ funciones2.py
@@ -0,0 +1,7 @@
+def saludar_y_sumar(nombre, num1, num2=0):
+    print("Saludos", nombre)
+    print("Resultado de la suma", num1+num2)
+
+saludar_y_sumar("monica", 15, 9)
+saludar_y_sumar("karla", 25)
+saludar_y_sumar("patricia", 26, 27)
